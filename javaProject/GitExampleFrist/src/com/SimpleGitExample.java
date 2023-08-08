e3ff556e64546072b42ca97a4137e56e8fcd7ccb
SADHANA PATIL <moresadhan5881@.com>
2023-08-08 12:23:50 +0530

    branches added

--- javaProject/GitExampleFrist/src/com/SimpleGitExample.java
+++ javaProject/GitExampleFrist/src/com/SimpleGitExample.java
@@ -5,7 +5,7 @@ public class SimpleGitExample {
 	public static void main(String[] args) {
 		System.out.println("Hi exponent");
 		
-		System.out.println("How are you student");
+		System.out.println("How are you student ");
 	}
 
 }
